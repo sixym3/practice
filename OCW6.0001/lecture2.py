318e319b4a80859c041c1ad5c183195e60400e9c
sixym3 <xiao.yuan0217@gmail.com>
2022-12-27 13:43:01 -0500

    Lecture note for lecture 1

---
+++ OCW6.0001/lecture2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 26 22:04:05 2022
+
+@author: sixym3
+"""
+
+"""
+hi = "hello there"
+name = "eric"
+greeting = hi + " " + name
+print(hi + name)
+print(greeting)
+print(hi + (" " + name) * 3)
+
+x = 1
+x_str = str(x)
+print("my num is", x, "x =", x)
+print("my num is", x_str, "x = " + x_str)
+"""
+
+text = input("type anything")
+print(text * 3)
+
+num = int(input("type a number"))
+print(num ** 2)
---
+++ python/tictactoe/main.py
@@ -0,0 +1,15 @@
+
+class tic_tac_toe():
+    def __init__(self):
+        pass
+    
+    def run():
+        pass
+    
+    def __exit__():
+        pass
+    
+    
+
+if __name__ == "__main__":
+    tic_tac_toe.run()
