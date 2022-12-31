b3bb44c90fce912c88138a055e89f7a01d40b9ec
sixym3 <xiao.yuan0217@gmail.com>
2022-12-31 11:56:54 -0500

    Working on lecture 6

--- OCW6.0001/lecture6.py
+++ OCW6.0001/lecture6.py
@@ -66,4 +66,94 @@ def factorial_iter(n):
         prod *= i
     return prod
 
-print(factorial_iter(3))
+def printmoves(n, fr, to):
+    print("Move disc",n ,"from" , fr, "to", to)
+
+def towers(n, fr, to, sp):
+    if n == 1:
+        printmoves(n, fr, to)
+    else:
+        towers(n - 1, fr, sp, to)
+        printmoves(n, fr, to)
+        towers(n - 1, sp, to, fr)
+        
+def fib(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+    
+def pali(inputstr):
+    n = len(inputstr)
+    if n <= 1:
+        return True
+    else:
+        return pali(inputstr[1:-1]) and inputstr[0] == inputstr[-1]
+    
+def isPalidrome(s):
+    
+    def toChars(s):
+        s = s.lower()   
+        ans = ""
+        for char in s:
+            if char in "abcdefghijklmnopqrstuvwxyz":
+                ans += char
+        return ans
+    
+    def ispali(s):
+        if len(s) <= 1:
+            return True
+        else:
+            return s[0] == s[-1] and ispali(s[1:-1])
+    
+    return ispali(toChars(s))
+
+def lyrics_to_dict(lyrics):
+    result = {}
+    for word in lyrics:
+        if word in result:
+            result[word] += 1
+        else:
+            result[word] = 1
+    return result
+
+def get_most_used_word(lyric_dict):
+    values = lyric_dict.values()
+    most_used = max(values)
+    result = []
+    for word in lyric_dict:
+        if lyric_dict[word] == most_used:
+            result.append(word)
+    return result
+
+song = """You're here where you should be
+Snow is falling as the carolers sing
+It just wasn't the same
+Alone on Christmas day
+Presents, what a beautiful sight
+Don't mean a thing if you ain't holding me tight
+You're all that I need
+Underneath the tree
+Tonight
+
+I'm gonna hold you close
+Make sure that you know
+I was lost before you
+Christmas was cold and grey
+Another holiday alone to celebrate
+But then, one day, everything changed
+You're all I need
+Underneath the tree
+""".split(" ")
+print(get_most_used_word(lyrics_to_dict(song)))
+    
+    
+#towers(4, "A", "B", "C")
+"""
+print(fib(6))
+for i in range(10):
+    print(fib(i))
+"""
+print(isPalidrome("Are	we	not	drawn	onward,	we	few,	drawn	onward	to	new	era?"))
+
+#print(factorial_iter(3))
