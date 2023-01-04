c6b291fb63a4fdf4ee2943311206c035799032e4
sixym3 <xiao.yuan0217@gmail.com>
2023-01-04 17:28:35 -0500

    ps1 part c update with working version, minor bug

--- OCW6.0001/ps1c.py
+++ OCW6.0001/ps1c.py
@@ -13,33 +13,37 @@ down_payment = total_cost * portion_down_payment
 epsilon = 100
 
 number_of_months = 36
+semi_annual_raise = .07
+r = 0.04
 
 best_saving_rate = 5000 # from 0 to 10000
 
-def savings_after_period(saving_rate, salary, period):
-    semi_annual_raise = .07
-    r = 0.04
+def savings_given_rate(saving_rate, salary):
     saving = 0
-    for i in range(period):
-        saving += (saving * r / 12)
-        saving += (saving_rate * salary / 12)
+    for i in range(number_of_months):
+        print("function: ", saving)
+        saving = saving * (1 + (r / 12))
+        saving = saving + (saving_rate * (salary / 12))
         if i % 6 == 0:
-            salary += (salary * semi_annual_raise)
+            salary = salary * (1 + semi_annual_raise)
+            
     return saving
 
-guess = 0
-upper = 10000
 lower = 0
-print(savings_after_period(best_saving_rate/100000.0, annual_salary, number_of_months))
+upper = 10000
 
-
-while abs(down_payment - guess) > epsilon:
-    print(lower)
-    if guess > down_payment:
-        lower = best_saving_rate
+def find_best_saving_rate(salary, lower, upper):
+    best_saving_rate = int((lower + upper) / 2)
+    print("rate:", best_saving_rate)
+    result = savings_given_rate(best_saving_rate/10000.0, salary)
+    if abs(result - down_payment) > epsilon:
+        if result > down_payment:
+            # return "greater"
+            return find_best_saving_rate(salary, lower, best_saving_rate)
+        else:
+            # return "less"
+            return find_best_saving_rate(salary, best_saving_rate, upper)
     else:
-        upper = best_saving_rate
-    best_saving_rate = (upper + lower) / 2
-    guess = savings_after_period(best_saving_rate/10000.0, annual_salary, number_of_months)
-
-print(best_saving_rate)
+        return best_saving_rate
+    
+print(find_best_saving_rate(annual_salary, lower, upper)/10000)
