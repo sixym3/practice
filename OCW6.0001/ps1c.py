eab99cc92134c7156194ffac48f58e12b948da96
sixym3 <xiao.yuan0217@gmail.com>
2023-01-05 09:28:36 -0500

    finished practice set 1 part c

--- OCW6.0001/ps1c.py
+++ OCW6.0001/ps1c.py
@@ -21,7 +21,7 @@ best_saving_rate = 5000 # from 0 to 10000
 def savings_given_rate(saving_rate, salary):
     saving = 0
     for i in range(number_of_months):
-        print("function: ", saving)
+        # print("function:", i, saving)
         saving = saving * (1 + (r / 12))
         saving = saving + (saving_rate * (salary / 12))
         if i % 6 == 0:
@@ -33,11 +33,13 @@ lower = 0
 upper = 10000
 
 def find_best_saving_rate(salary, lower, upper):
-    best_saving_rate = int((lower + upper) / 2)
-    print("rate:", best_saving_rate)
+    global counter
+    counter += 1
+    best_saving_rate = round((lower + upper) / 2.0)
+    # print("rate:", best_saving_rate)
     result = savings_given_rate(best_saving_rate/10000.0, salary)
     if abs(result - down_payment) > epsilon:
-        if result > down_payment:
+        if result >= down_payment:
             # return "greater"
             return find_best_saving_rate(salary, lower, best_saving_rate)
         else:
@@ -46,4 +48,10 @@ def find_best_saving_rate(salary, lower, upper):
     else:
         return best_saving_rate
     
-print(find_best_saving_rate(annual_salary, lower, upper)/10000)
+counter = 0
+try:
+    result = (find_best_saving_rate(annual_salary, lower, upper)/10000.0)
+    print("Best saving rate:", result)
+    print("Steps in bisection search", counter)
+except RecursionError:
+    print("It is not possible to pay the down payment in three years")
